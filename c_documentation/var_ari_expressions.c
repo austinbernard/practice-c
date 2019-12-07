d49c4180e4c39714846264f55062fc0715fabb20
austinbernard <contactausbernard@gmail.com>
2019-12-07 13:49:03 -0500

    Update var_ari_expressions.c

--- c_documentation/var_ari_expressions.c
+++ c_documentation/var_ari_expressions.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
-/* print Farenheight-Celsius table for far = 0, 20, ..., 300 */
-main (){
+int main()
+{
+    int fahr, celsius;		/* variable must be declared before use */
+    int lower, upper, step;	/* variable must be declared before use */
+    
+    lower = 0;      /* lower limit of temperature table */
+    upper = 300;    /* upper lmit */
+    step = 20;      /* step size */
+    
+    fahr = lower;
+    while (fahr <= upper) {
+        celsius = 5 * (fahr-32) / 9;
+        printf("%d\t%d\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+    return 0;
+}
 
