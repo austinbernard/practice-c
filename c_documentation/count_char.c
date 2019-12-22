2c37d967a35220c4e285dc9fa1ecd11b8aab7bf3
austinbernard <contactausbernard@gmail.com>
2019-12-22 11:00:13 -0500

    update test

---
+++ c_documentation/count_char.c
@@ -0,0 +1,30 @@
+https://www.youtube.com/watch?v=iOq5kSKqeR4
+
+//Count the number of characters in a string
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+    
+    char a[100];
+    int length;
+    
+    printf("Enter a string: \n");
+    gets(a)
+    
+    length = strlen(a);
+    
+    printf("Length of entered string is = %d\n", length)
+    
+    return 0
+
+}
+  
+
+//input: "yes yes yes yes yes"
+//output: 19
+
+https://www.quora.com/How-do-I-count-the-length-of-string-in-C
+
+
---
+++ dartmouth_c01/warmup_2_1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(void) {
+    int sunny = 1;
+    int vacation = 1;
+    return 0;
+
+}
