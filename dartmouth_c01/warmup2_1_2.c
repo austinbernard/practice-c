e7d30c4b2511a95a4be2ec38697c3d375b659a7f
austinbernard <contactausbernard@gmail.com>
2019-12-22 11:22:09 -0500

    update wu2.1_2

--- dartmouth_c01/warmup2_1_2.c
+++ dartmouth_c01/warmup2_1_2.c
@@ -1,9 +1,10 @@
+// OR operator
 #include <stdio.h>
 int main(void) {
     int sunny = 1;
     int vacation = 1;
     int sunORVacation = sunny || vacation;
-    // 1 && 1 : 1, 1 && 0 : 0, 0 && 1 : 0, 0 && 0 : 0
+    // 1 || 1 : 1, 1 || 0 : 0, 0 || 1 : 0, 0 || 0 : 0
     // to go to else both variables must be 0 
     if (sunORVacation) {
         printf("Going well: it is sunny OR I am on vacation OR both!\n");
